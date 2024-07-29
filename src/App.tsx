f768a7f67304ea8f8cc8b1d39d0a5189a4f70f86
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-29 17:24:34 +0200

    Basic experimentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-export function App() {
+import { ReactElement } from "react";
+import { Navbar} from "./components/Navbar";
+
+export function App(): ReactElement {
+
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =><li>{number}</li>);
+
   return (
     <>
+      <Navbar/>
       <h1>This is the App component</h1>
+      <ul>{listItems}</ul>
     </>
   );
 }
+
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import "./Navbar.css";
+
+export function Navbar(): ReactElement {
+    return (
+      <nav className="navbar">
+        dasdas
+      </nav>
+    );
+  }
