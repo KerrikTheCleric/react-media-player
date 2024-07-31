533aa4c1ef050c4d6987d42ba37dc37831c61dab
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-31 10:59:43 +0200

    Rough track list complete

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -9,19 +9,31 @@ import "./TrackListItem.css";
 
 export function GridContainer({ data }: ITrackMenuItemList): ReactElement {
 
-    const numbers = [1, 2, 3, 4, 5];
+    /*const numbers = [1, 2, 3, 4, 5];
     const doubled = numbers.map((number) => number * 2);
-    console.log(doubled);
+    console.log(doubled);*/
 
     const listItems = data.map((track) =>
-        <li className="no-bullets">{track.artist}</li>
+        <li className="trackListItem">
+            <img className="list-image"
+                src={track.image}
+                alt={track.name}
+            />
+            <ul className="trackList">
+                <li><b>{track.artist}</b></li>
+                <li><b>{track.name}</b></li>
+                <b>{track.name}</b>
+            </ul>
+            
+            <span className="material-symbols-outlined"> play_circle </span>
+        </li>
     );
 
 
     return (
 
         <div className="grid-container">
-             <ul>{listItems}</ul>
+             <ul className="trackList">{listItems}</ul>
             <div className="item2">{data[0].name}</div>
         </div>
     );
